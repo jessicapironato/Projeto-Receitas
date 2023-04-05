a8d4b2011ec1d6eaadd6d58c04b044ffa445f103
André Porto <112976026+andrevporto@users.noreply.github.com>
2023-04-05 18:52:14 -0300

    alterações nos testes para subir o coverage

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -9,6 +9,7 @@ describe('Testes Login', () => {
   const password = 'coxinha123';
   const emailTestID = 'email-input';
   const passwordTestID = 'password-input';
+  const searchTopBtn = 'search-top-btn';
 
   it('1.Testa inputs', () => {
     const { history } = renderWithRouterAndRedux(<App />);
@@ -55,12 +56,19 @@ describe('Testes Login', () => {
 
     expect(history.location.pathname).toBe('/meals');
 
-    const searchIcon = screen.getByTestId('search-top-btn');
-    userEvent.click(searchIcon);
+    const searchIcon = screen.getByTestId(searchTopBtn);
     expect(searchIcon).toBeVisible();
+    userEvent.click(searchIcon);
+
+    waitFor(() => {
+      const searchInput = screen.getByTestId(searchTopBtn);
+      expect(searchInput).toBeVisible();
+      userEvent.click(searchIcon);
+      expect(searchInput).not.toBeVisible();
+    });
   });
 
-  it('3.Testa SearchBar', async () => {
+  it('3.Testa SearchBar na rota /meals', async () => {
     const { history } = renderWithRouterAndRedux(<App />);
 
     const buttonLogin = screen.getByRole('button', { name: 'Enter' });
@@ -72,7 +80,7 @@ describe('Testes Login', () => {
 
     expect(history.location.pathname).toBe('/meals');
 
-    const buttonSearchBar = screen.getByTestId('search-top-btn');
+    const buttonSearchBar = screen.getByTestId(searchTopBtn);
     expect(buttonSearchBar).toBeInTheDocument();
 
     userEvent.click(buttonSearchBar);
@@ -82,8 +90,12 @@ describe('Testes Login', () => {
       expect(searchInput.toBeVisible());
       userEvent.type(buttonSearchBar, inputSearch);
       const ingredientRadioButton = screen.getByTestId('ingredient-search-radio');
+      const firstLetterRadioButton = screen.getByTestId('first-letter-search-radio');
       expect(ingredientRadioButton).toBeInTheDocument();
+      expect(ingredientRadioButton).not.toBeChecked();
       userEvent.selectOptions(ingredientRadioButton);
+      expect(ingredientRadioButton).toBeChecked();
+      expect(firstLetterRadioButton).not.toBeChecked();
     });
 
     const mockAlert = jest.spyOn(global, 'alert').mockImplementation(() => 'Your search must have only 1 (one) character');
@@ -93,8 +105,54 @@ describe('Testes Login', () => {
     userEvent.click(firstLetterRadioButton);
     userEvent.click(buttonSearchFilter);
     waitFor(() => {
-      expect(mockAlert).toHaveBeenCalledTimes(1);
+      expect(mockAlert).not.toHaveBeenCalled();
       mockAlert.mockRestore();
+    });
+
+    const mockAlertNotFound = jest.spyOn(global, 'alert').mockImplementation(() => 'Sorry, we haven\'t found any recipes for these filters.');
+    waitFor(() => {
+      expect(searchInput.toBeVisible());
+      userEvent.type(buttonSearchBar, inputSearch);
+      const nameRadioButton = screen.getByTestId('name-search-radio');
+      expect(nameRadioButton).toBeInTheDocument();
+      userEvent.click(nameRadioButton);
+      userEvent.click(buttonSearchFilter);
+      expect(mockAlertNotFound).toHaveBeenCalledTimes(1);
+      mockAlertNotFound.mockRestore();
+    });
+  });
+
+  it('4.Testa SearchBar na rota /drink', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    const buttonLogin = screen.getByRole('button', { name: 'Enter' });
+    const emailInput = screen.getByTestId(emailTestID);
+    const passwordInput = screen.getByTestId(passwordTestID);
+    userEvent.type(emailInput, email);
+    userEvent.type(passwordInput, password);
+    userEvent.click(buttonLogin);
+
+    const buttonDrink = screen.getByTestId('drinks-bottom-btn');
+
+    userEvent.click(buttonDrink);
+
+    expect(history.location.pathname).toBe('/drinks');
+
+    const buttonSearchBar = screen.getByTestId(searchTopBtn);
+    expect(buttonSearchBar).toBeInTheDocument();
+
+    userEvent.click(buttonSearchBar);
+    const searchInput = await screen.findByTestId('search-input');
+    const inputDrink = 'vodka';
+    const inputDrink2 = 'gin';
+
+    waitFor(() => {
+      userEvent.type(searchInput, inputDrink);
+      expect(screen.getByText('vodka')).toBeInTheDocument();
+    });
+    waitFor(() => {
+      userEvent.type(searchInput, inputDrink2);
+      expect(screen.getByText('gin')).toBeInTheDocument();
     });
   });
 });
