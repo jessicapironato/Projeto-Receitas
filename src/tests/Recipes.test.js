2185f55b99c1883c21acf737784a2ca109a7ba31
PFonsecaFV <pfonsecafv@gmail.com>
2023-04-12 12:47:58 -0300

    Requisitos - Tests em Progresso (Lint)

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import userEvent from '@testing-library/user-event';
-import { screen } from '@testing-library/react';
-import renderWithRouterAndRedux from './utils/rwrar';
-import fetch from '../../cypress/mocks/fetch';
-import App from '../App';
+// import React from 'react';
+// import userEvent from '@testing-library/user-event';
+// import { screen } from '@testing-library/react';
+// import renderWithRouterAndRedux from './utils/rwrar';
+// import fetch from '../../cypress/mocks/fetch';
+// import App from '../App';
 
 describe('.: Testa component <Recipes> :.', () => {
   describe('Depende do funcionamento completo do botão FINISH RECIPE tanto em Meals e Drinks', () => {
